07da34163d93a1a37a3aee20bb02b21e88f3d67e
daisuke1418 <daisuke.mdv.vbm@icloud.com>
2021-07-05 11:52:03 +0900

    models_room && userのmany修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,6 @@ class User < ApplicationRecord
 
   validates :name, presence: true
 
-  has_meny :room_users
-  has_meny :rooms, through: :room_users
+  has_many :room_users
+  has_many :rooms, through: :room_users
 end
